8fe2eaa5b5d220a1dae8bd145063c54927ce743c
jaydcarvajal <jaydcarvajal@gmail.com>
2020-11-10 20:15:52 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$("#one").delay(1000).fadeIn();
+$("#two").delay(3000).fadeIn();
+$("#three").delay(5000).fadeIn();
+$("#img-one").delay(2000).fadeIn();
+$("#img-two").delay(4000).fadeIn();
+$("#img-three").delay(6000).fadeIn();
+
